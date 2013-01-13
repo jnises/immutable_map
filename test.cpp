0098a6a08baa95c26bc302fa1e7d4706e46be7c6
Joel Nises <joel@nises.se>
2013-01-13 21:02:04 +0100

    moved tests around

--- test.cpp
+++ test.cpp
@@ -5,21 +5,15 @@
 
 using namespace deepness;
 
-int main(int argc, char *argv[])
+void test0()
 {
-    std::cout << "immutable_map tests" << std::endl;
     immutable_map<int, const int> intmap;
     auto intmap2 = intmap.set(0, 42);
     assert(intmap2[0] == 42);
-
-    auto intmap3 = intmap2.set(0, 1);
-    assert(intmap3[0] == 1);
-
-    intmap3 = intmap2.erase(0);
     bool exception = false;
     try
     {
-        intmap3[0];
+        intmap[0];
     }
     catch(key_error &)
     {
@@ -27,24 +21,61 @@ int main(int argc, char *argv[])
     }
     assert(exception);
 
-    immutable_map<int, const int> intmap4;
+    auto intmap3 = intmap2.set(0, 1);
+    assert(intmap3[0] == 1);
+}
+
+void test1()
+{
+    immutable_map<int, const int> intmap;
+    intmap = intmap.set(0, 1);
+    assert(intmap[0] == 1);
+    intmap = intmap.erase(0);
+    bool exception = false;
+    try
+    {
+        intmap[0];
+    }
+    catch(key_error &)
+    {
+        exception = true;
+    }
+    assert(exception);
+}
+
+void test2()
+{
+    immutable_map<int, const int> intmap;
     for(size_t i = 0; i < 10000; ++i)
     {
-        intmap4 = intmap4.set(i, i);
-		assert(intmap4[i] == i);
+        intmap = intmap.set(i, i);
+		assert(intmap[i] == i);
     }
 
     for(size_t i = 0; i < 10000; ++i)
     {
-        assert(intmap4[i] == i);
+        assert(intmap[i] == i);
     }
+}
 
-	immutable_map<int, int> intmap5;
-	intmap5 = intmap5.set(0, 0);
-	assert(intmap5[0] == 0);
+void test3()
+{
+	immutable_map<int, int> intmap;
+	intmap = intmap.set(0, 0);
+	assert(intmap[0] == 0);
 	// since we return a non-const reference this is possible
-	intmap5[0] = 4;
-	assert(intmap5[0] == 4);
+	intmap[0] = 4;
+	assert(intmap[0] == 4);
+}
+
+int main(int argc, char *argv[])
+{
+    std::cout << "immutable_map tests" << std::endl;
+
+    test0();
+    test1();
+    test2();
+    test3();
 
     std::cout << "tests complete" << std::endl;
 }
